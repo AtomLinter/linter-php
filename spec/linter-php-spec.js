34e9a53b08a7e100a999b4f1aa8f78f4e11671c5
Landon Abney <Landonabney@gmail.com>
2016-01-26 00:30:06 -0800

    Fix linting issues

--- spec/linter-php-spec.js
+++ spec/linter-php-spec.js
@@ -1,6 +1,12 @@
 'use babel';
 /* eslint-env jasmine */
 
+import * as path from 'path';
+
+const badPath = path.join(__dirname, 'files', 'bad.php');
+const goodPath = path.join(__dirname, 'files', 'good.php');
+const emptyPath = path.join(__dirname, 'files', 'empty.php');
+
 describe('The php -l provider for Linter', () => {
   const lint = require('../lib/main').provideLinter().lint;
 
@@ -9,40 +15,40 @@ describe('The php -l provider for Linter', () => {
     waitsForPromise(() => {
       atom.packages.activatePackage('linter-php');
       return atom.packages.activatePackage('language-php').then(() =>
-        atom.workspace.open(__dirname + '/files/bad.php')
+        atom.workspace.open(badPath)
       );
     });
   });
 
-  it('should be in the packages list', () => {
-    return expect(atom.packages.isPackageLoaded('linter-php')).toBe(true);
-  });
+  it('should be in the packages list', () =>
+    expect(atom.packages.isPackageLoaded('linter-php')).toBe(true)
+  );
 
-  it('should be an active package', () => {
-    return expect(atom.packages.isPackageActive('linter-php')).toBe(true);
-  });
+  it('should be an active package', () =>
+    expect(atom.packages.isPackageActive('linter-php')).toBe(true)
+  );
 
   describe('checks bad.php and', () => {
     let editor = null;
     beforeEach(() => {
-      waitsForPromise(() => {
-        return atom.workspace.open(__dirname + '/files/bad.php').then(openEditor => {
+      waitsForPromise(() =>
+        atom.workspace.open(badPath).then(openEditor => {
           editor = openEditor;
-        });
-      });
+        })
+      );
     });
 
     it('finds at least one message', () => {
-      waitsForPromise(() => {
-        return lint(editor).then(messages => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
           expect(messages.length).toEqual(1);
-        });
-      });
+        })
+      );
     });
 
     it('verifies that message', () => {
-      waitsForPromise(() => {
-        return lint(editor).then(messages => {
+      waitsForPromise(() =>
+        lint(editor).then(messages => {
           expect(messages[0].type).toBeDefined();
           expect(messages[0].type).toEqual('Error');
           expect(messages[0].text).toBeDefined();
@@ -52,28 +58,28 @@ describe('The php -l provider for Linter', () => {
           expect(messages[0].range).toBeDefined();
           expect(messages[0].range.length).toEqual(2);
           expect(messages[0].range).toEqual([[1, 0], [1, 6]]);
-        });
-      });
+        })
+      );
     });
   });
 
   it('finds nothing wrong with an empty file', () => {
-    waitsForPromise(() => {
-      return atom.workspace.open(__dirname + '/files/empty.php').then(editor => {
-        return lint(editor).then(messages => {
+    waitsForPromise(() =>
+      atom.workspace.open(emptyPath).then(editor =>
+        lint(editor).then(messages => {
           expect(messages.length).toEqual(0);
-        });
-      });
-    });
+        })
+      )
+    );
   });
 
   it('finds nothing wrong with a valid file', () => {
-    waitsForPromise(() => {
-      return atom.workspace.open(__dirname + '/files/good.php').then(editor => {
-        return lint(editor).then(messages => {
+    waitsForPromise(() =>
+      atom.workspace.open(goodPath).then(editor =>
+        lint(editor).then(messages => {
           expect(messages.length).toEqual(0);
-        });
-      });
-    });
+        })
+      )
+    );
   });
 });
